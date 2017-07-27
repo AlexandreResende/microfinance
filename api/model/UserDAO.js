68232723b8836324958703e55753cd5ffd60a6e4
Alexandre Resende <amaralr@alexandres-mbp.br.ibm.com>
2017-07-27 11:19:36 -0300

    refactor: Adjust indentation

--- api/model/UserDAO.js
+++ api/model/UserDAO.js
@@ -9,8 +9,16 @@ UserDAO.prototype.signUp = function(req, res, userInfo) {
     this._connection.collection('user').find({username: userInfo.username}).toArray((err, users) => {
 
         if (err){
-            return res.status(500).render('index', {error: err});
+
+            return res.status(500).render('index', {
+                                                    validation: "",
+                                                    ok: "",
+                                                    error: err,
+                                                    userData: ""
+                                                   });
+
         } else if (users.length !== 0){
+
             //precondition failed
             return res.status(412).render('index', {
                                                     validation: "", 
@@ -18,18 +26,22 @@ UserDAO.prototype.signUp = function(req, res, userInfo) {
                                                     error: `This username is already in use.`, 
                                                     userData: ""
                                                    });
+
         }
 
         this._connection.collection('user').find({email: userInfo.email}).toArray((err, users) => {
 
             if (err){
+
                 return res.status(500).render('index', {
                                                         validation: "", 
                                                         ok: "", 
                                                         error: err, 
                                                         userData: ""
                                                        });
+
             } else if (users.length !== 0){
+
                 //precondition failed
                 return res.status(412).render('index', {
                                                         validation: "", 
@@ -37,6 +49,7 @@ UserDAO.prototype.signUp = function(req, res, userInfo) {
                                                         error: `This email is already in use.`, 
                                                         userData: ""
                                                        });
+
             }
 
             this._connection.collection('user').insert(userInfo, (err, result) => {
@@ -45,18 +58,26 @@ UserDAO.prototype.signUp = function(req, res, userInfo) {
                 let id;
                 
                 if (err){
-                    return res.status(500).render('error', {error: err});
+
+                    return res.status(500).render('error', {
+                                                            validation: "",
+                                                            ok: "",
+                                                            error: err,
+                                                            userData: ""
+                                                           });
+
                 }
 
                 username = userInfo.username;
                 id = result.ops[0]._id;
 
                 return res.status(200).render('index', {
-                                            validation: "",
-                                            ok: `User signed up successfully.`,
-                                            error: "", 
-                                            userData: {id: id, username: username}
-                                           });
+                                                        validation: "",
+                                                        ok: `User signed up successfully.`,
+                                                        error: "", 
+                                                        userData: {id: id, username: username}
+                                                       });
+
             });
 
         });
