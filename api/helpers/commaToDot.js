a82ecc6d16da7a42177e88169336724d0d36088c
Alexandre Resende <alexandre.amres@gmail.com>
2017-08-13 08:55:22 -0300

    feat: Add helper function to substitute the comma to dot in float point numbers

---
+++ api/helpers/commaToDot.js
@@ -0,0 +1,9 @@
+function dotToComma(value){
+
+    let newValue = arguments[0] + "." + arguments[1];
+
+    return newValue;
+
+}
+
+module.exports = commaToDot;
--- api/helpers/helper.js
+++
@@ -1,1 +0,0 @@
-//some helper function here
