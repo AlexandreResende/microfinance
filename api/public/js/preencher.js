e917c924485d0175c6fb0908f60f892cc7a19740
jonathan <jonathanbustillos@hotmail.com>
2017-08-26 13:00:08 -0300

    func: Add page [receitas] and update script [preencher]

--- api/public/js/preencher.js
+++ api/public/js/preencher.js
@@ -1,7 +1,14 @@
+/***************************************************************************
+* ATIVA O USO DOS <SELECTS>
+****************************************************************************/
 $(document).ready(function() {
     $('select').material_select();
   });
 
+/***************************************************************************
+* ESTA FUNÇÃO É SÓ PARA A PÁGINA HOME DEVIDO AOS MODAIS QUE TEM PARA
+* PREENCHER OS <SELECTS>
+****************************************************************************/
 function preencheAno() {
 	var init = 2010;
 	var date = new Date();
@@ -17,5 +24,26 @@ function preencheAno() {
 		elem.text = init;
 		comboAnosReceita.add(element, comboAnosReceita.options[init]);
 		comboAnosDespesa.add(elem, comboAnosDespesa.options[init]);
+	}	
+}
+
+/***************************************************************************
+* ESTA FUNÇÃO É SÓ PARA AS PÁGINAS RECEITA E DESPESA
+****************************************************************************/
+function ano() {
+	var init = 2010;
+	var date = new Date();
+	var yearCurrent = date.getFullYear();
+	var comboAnos = document.getElementById("cbAno");
+	var cbAnosModal = document.getElementById("cbAnosModal");
+	for (init ; init <= yearCurrent; init++) {
+		var element = document.createElement("option");
+		var elem = document.createElement("option");
+		element.value = init;
+		element.text = init;
+		elem.value = init;
+		elem.text = init
+		comboAnos.add(element, comboAnos.options[init]);
+		cbAnosModal.add(elem, cbAnosModal.options[init]);
 	}	
 }
