990d8d744a01f5bb9e1b62a898fdd1f166d1f1bb
Alexandre Resende <aar6947@gmail.com>
2017-10-08 15:25:06 -0300

    refactor: Substitute callbacks to promises on ExpenseDAO

--- api/model/ExpenseDAO.js
+++ api/model/ExpenseDAO.js
@@ -8,22 +8,38 @@ function ExpenseDAO(connection){
 
 ExpenseDAO.prototype.getAllExpenses = function(req, res){
 
+    let getExpenses;
     let userId = {
                   ownerId: ObjectId('597fccdb5623c9b346c275fd')//req.session.userId
                  };
 
     let expensesColl = this._connection.collection('expenses');
 
-    expensesColl.find(userId).toArray((err, userExpensesResult) => {
+    getExpenses = new Promise((resolve, reject) => {
 
-        if (err){
+        expensesColl.find(userId).toArray((err, userExpensesResult) => {
+            
+            if (err){
 
-            res.status(500).send({error: `An error occurred.`});
+                reject(err);
 
-        }
+            }
+    
+            resolve(userExpensesResult);
+    
+        });
 
-        //create a function to sort the Expenses by year and month
+    });
+
+    getExpenses
+    .then((userExpensesResult) => {
+
         res.status(200).send({msg: `Returned all Expenses successfully`, result: userExpensesResult});
+
+    })
+    .catch((err) => {
+
+        res.status(500).send({error: `An error occurred.`});
 
     });
 
@@ -31,35 +47,69 @@ ExpenseDAO.prototype.getAllExpenses = function(req, res){
 
 ExpenseDAO.prototype.insertExpenses = function(req, res, expenseInfo){
 
+    let insertExpenses;
     let expensesColl = this._connection.collection('expenses'); 
 
-    expensesColl.insert(expenseInfo, (err, insertResult) => {
+    insertExpenses = new Promise((reject, resolve) => {
 
-        if (err){
+        expensesColl.insert(expenseInfo, (err, insertResult) => {
+            
+            if (err){
+    
+                reject(err);
+    
+            }
+    
+            resolve(insertResult.ops[0]);
+    
+        });   
 
-            return es.status(500).send({error: `An error occurred. ${err}`});
+    });
 
-        }
+    insertExpenses
+    .then((insertResult) => {
 
-        return res.status(201).send({msg: `Expense inserted`, result: insertResult.ops[0]});
+        return res.status(201).send({msg: `Expense inserted`, result: insertResult});
 
-    });   
+    })
+    .catch((err) => {
+
+        return res.status(500).send({error: `An error occurred. ${err}`});
+
+    });
 
 }
 
 ExpenseDAO.prototype.updateExpenses = function(req, res, ExpenseId, updateInfo){
 
+    let updateExpenses;
     let expensesColl = this._connection.collection('expenses');
 
-    expensesColl.update(ExpenseId, {$set: updateInfo}, (err, updateResult) => {
+    updateExpenses = new Promise((reject, resolve) => {
 
-        if (err){
+        expensesColl.update(ExpenseId, {$set: updateInfo}, (err, updateResult) => {
+            
+            if (err){
+    
+                reject(err);
+    
+            }
+    
+            resolve(updateResult);
+    
+        });
 
-            return es.status(500).send({error: `An error occurred. ${err}`});
+    });
 
-        }
+    updateExpenses
+    .then((updateResult) => {
 
         return res.status(200).send({msg: `Expense updated`, result: updateResult});
+
+    })
+    .catch((err) => {
+
+        return res.status(500).send({error: `An error occurred. ${err}`});
 
     });
 
@@ -67,17 +117,34 @@ ExpenseDAO.prototype.updateExpenses = function(req, res, ExpenseId, updateInfo){
 
 ExpenseDAO.prototype.removeExpenses = function(req, res, ExpenseId){
 
+    let removeExpenses;
     let expensesColl = this._connection.collection('expenses');
 
-    expensesColl.remove(ExpenseId, (err, removeResult) => {
+    removeExpenses = new Promise((resolve, reject) => {
 
-        if (err){
+        expensesColl.remove(ExpenseId, (err, removeResult) => {
+            
+            if (err){
+    
+                reject(err);
+    
+            }
+    
+            resolve(removeResult);
+    
+        });
 
-            return es.status(500).send({error: `An error occurred. ${err}`});
+    });
 
-        }
+    removeExpenses
+    .then((removeResult) => {
 
         return res.status(200).send({msg: `Expense removed`, result: removeResult});
+
+    })
+    .catch((err) => {
+
+        return res.status(500).send({error: `An error occurred. ${err}`});
 
     });
 
