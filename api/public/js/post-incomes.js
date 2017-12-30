d91788d754c45180d5eb02f2632b7edc9c14ec9d
Alexandre Resende <aar6947@gmail.com>
2017-12-30 00:50:25 -0200

    refactor: Add convert to json function based on form

--- api/public/js/post-incomes.js
+++ api/public/js/post-incomes.js
@@ -1,15 +1,25 @@
 $(document).ready(function(){
+
+    const convertToJson = (string) => {
+        const stringArray = string.split('&');
+        const result = {};
+
+        for (const str of stringArray) {
+            splittedString = str.split('=');
+            result[splittedString[0]] = splittedString[1];      
+        }
+        return JSON.parse(JSON.stringify(result));
+    }
+
     $("#btnIncomes").on('click', function(){
-        console.log('AKI');
-        console.log(JSON.stringify($("#formIncomes")));
         $.ajax({
             type: "POST",
-            dataType: "application/json",
+            dataType: "text",
             url: 'http://localhost:10010/5a350ddef0439d0684137eaa/incomes',
-            data: $("#formIncomes").serialize(),
+            data: convertToJson(($("#formIncomes").serialize())),
         })
         .done(function(data) {
-            console.log("Realizado a inserção com sucesso!" + data);
+            console.log("Realizado a inserção com sucesso!");
         })
         .fail(function(jqXHR, text, error) {
             console.log("Erro: "+ error + "\nText: " + text + "\njqXHR: " + jqXHR);
